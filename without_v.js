9366aeff5ce30f2942b2e3f51bd96f6a689fa357
Yuvaraj A <104821565+git-yuvi@users.noreply.github.com>
2023-11-03 01:29:53 +0530

    Create without_v.js

---
+++ without_v.js
@@ -0,0 +1,33 @@
+// Function to filter and display batsmen and all-rounders without wickets
+function displayBatsmenAndAllroundersWithoutWickets(data) {
+  data.forEach((country) => {
+    console.log(`Country: ${country.country}`);
+    
+    // Display Batsmen
+    console.log('Batsmen:');
+    country.batsman.forEach((player) => {
+      const { name, formats } = player;
+      const { test, odi, t20 } = formats;
+      console.log(`Name: ${name}`);
+      console.log(`Test: Matches: ${test.matches}, Innings: ${test.innings}, Runs: ${test.runs}, Avg: ${test.avg}`);
+      console.log(`ODI: Matches: ${odi.matches}, Innings: ${odi.innings}, Runs: ${odi.runs}, Avg: ${odi.avg}`);
+      console.log(`T20: Matches: ${t20.matches}, Innings: ${t20.innings}, Runs: ${t20.runs}, Avg: ${t20.avg}`);
+    });
+    
+    // Display All-rounders without wickets
+    console.log('All-rounders:');
+    country.allrounders.forEach((player) => {
+      const { name, formats } = player;
+      const { test, odi, t20 } = formats;
+      console.log(`Name: ${name}`);
+      console.log(`Test: Matches: ${test.matches}, Innings: ${test.innings}, Runs: ${test.runs}, Avg: ${test.avg}`);
+      console.log(`ODI: Matches: ${odi.matches}, Innings: ${odi.innings}, Runs: ${odi.runs}, Avg: ${odi.avg}`);
+      console.log(`T20: Matches: ${t20.matches}, Innings: ${t20.innings}, Runs: ${t20.runs}, Avg: ${t20.avg}`);
+    });
+
+    console.log('\n');
+  });
+}
+
+// Call the function to display the data
+displayBatsmenAndAllroundersWithoutWickets(stats);
