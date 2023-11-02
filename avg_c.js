3b153359a8be4185a0d9e3dcc76b5ad17e56cc5f
Yuvaraj A <104821565+git-yuvi@users.noreply.github.com>
2023-11-03 01:28:27 +0530

    Create avg_c.js

---
+++ avg_c.js
@@ -0,0 +1,20 @@
+const allBatsmen = stats.reduce((acc, country) => {
+   return acc.concat(country.batsman);
+ }, []);
+ 
+ const sortedBatsmen = allBatsmen.sort((a, b) => {
+   return b.formats.test.avg - a.formats.test.avg;
+ });
+ 
+ // Display the sorted batsman details
+ sortedBatsmen.forEach((batsman, index) => {
+   console.log(`#${index + 1}`);
+   console.log(`Name: ${batsman.name}`);
+   console.log(`Country: ${stats.find((country) => country.batsman.includes(batsman)).country}`);
+   console.log("Test Stats:");
+   console.log(`  Matches: ${batsman.formats.test.matches}`);
+   console.log(`  Innings: ${batsman.formats.test.innings}`);
+   console.log(`  Runs: ${batsman.formats.test.runs}`);
+   console.log(`  Average: ${batsman.formats.test.avg}`);
+   console.log("\n");
+ });
