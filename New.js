85fecc52d8ad251e74d5aa1a1a4983991cd43ae8
Yuvaraj A <104821565+git-yuvi@users.noreply.github.com>
2023-11-02 17:18:53 +0530

    Create New.js

---
+++ New.js
@@ -0,0 +1,23 @@
+cricketData.forEach((team) => {
+    team.batsman.sort((a, b) => {
+        if (a.formats.test.avg < b.formats.test.avg) {
+            return -1;
+        } else if (a.formats.test.avg > b.formats.test.avg) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+});
+
+cricketData.forEach((team) => {
+    team.batsman.sort((a, b) => {
+        if (a.formats.test.avg > b.formats.test.avg) {
+            return -1;
+        } else if (a.formats.test.avg < b.formats.test.avg) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+});
