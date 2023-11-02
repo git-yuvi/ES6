906611a26b0493aa856a0524e93419a3de1d4dff
Yuvaraj A <104821565+git-yuvi@users.noreply.github.com>
2023-11-03 01:32:45 +0530

    Create newhtml.js

---
+++ newhtml.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Cricket Stats</title>
+</head>
+<body>
+  <button id="batsmanBtn">Batsman</button>
+  <button id="bowlerBtn">Bowler</button>
+  <button id="resetBtn">Reset</button>
+  <br>
+  <button id="sortAverageBtn">Sort Batsman by Test Average</button>
+  <button id="noWicketsBtn">Show Batsman and Allrounders without Wickets</button>
+  <br>
+  <pre id="output"></pre>
+  <script src="json5.js"></script>
+</body>
+</html>
